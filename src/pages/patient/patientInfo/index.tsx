0930cb1fc305cb42636f49de8c1687fbff4c1492
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-05-29 23:10:58 +0300

    Update index.tsx

--- src/pages/patient/patientInfo/index.tsx
+++ src/pages/patient/patientInfo/index.tsx
@@ -11,7 +11,7 @@ const PatientInfo = () => {
     <>
       <GoBackLink />
       <ZoomComponent />
-      <MeetNotification />
+      {/* <MeetNotification /> */}
       <PatientCardInfo />
       <WeeklyCalendar />
       <Notes />
