74e0cafcfadae590d489ee644cd4a0e5559c00e9
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-06-08 21:50:57 +0300

    Update index.ts

--- src/redux/slices/socketAppointmentsSlice/index.ts
+++ src/redux/slices/socketAppointmentsSlice/index.ts
@@ -2,7 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   nextAppointment:{
-    //  nextAppointment: {
       endTime: '',
       id: null,
       localDoctor: { id: null, firstName: '', lastName: '' },
@@ -10,7 +9,6 @@ const initialState = {
       remoteDoctor: { id: null, firstName: '', lastName: '' },
       startTime: '',
       zoomLink: '',
-    // },
   },
   callConfig: {
     name: '',
